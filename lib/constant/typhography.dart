cab40408d0f2ff8aa5b880fbf157731c2f4d4b73
= <=>
2020-04-04 22:02:56 +0800

    add folder , bloc , repo untuk clean arc

--- lib/constant/typhography.dart
+++ lib/constant/typhography.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-Widget headingTextBold({@required String text, @required Color color, TextAlign textAlign = TextAlign.left}) {
+Widget headingTextBold(
+    {@required String text,
+    @required Color color,
+    TextAlign textAlign = TextAlign.left}) {
   return Text(
     text,
     style: TextStyle(color: color, fontWeight: FontWeight.w600, fontSize: 36),
@@ -8,7 +11,10 @@ Widget headingTextBold({@required String text, @required Color color, TextAlign 
   );
 }
 
-Widget headingTextMedium({@required String text, @required Color color, TextAlign textAlign = TextAlign.left}) {
+Widget headingTextMedium(
+    {@required String text,
+    @required Color color,
+    TextAlign textAlign = TextAlign.left}) {
   return Text(
     text,
     style: TextStyle(
