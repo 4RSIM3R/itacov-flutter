45002a12c237197f12a0155ea797c43cacf63a68
= <=>
2020-04-05 00:42:56 +0800

    Build Home Screen and Add Some assets

--- lib/repo/corona_repo.dart
+++ lib/repo/corona_repo.dart
@@ -1,4 +1,28 @@
 // Tambahkan Api Yang Akan Di Consume Ke Bloc
+import 'package:dio/dio.dart';
 
+class CoronaRepo {
+  static String baseUrl = "https://api.kawalcorona.com/";
+  final Dio _dio = Dio();
+  var getProvinsi = "$baseUrl/indonesia/provinsi";
 
-class CoronaRepo {}
+  // Ambil Ke Api Secara Global
+  Future<Response> getGlobal() async {
+    try {
+      Response response = await _dio.get(baseUrl);
+      // harus nya Ini Di Ambil Dari Model Data
+      return response.data;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  Future<Response> getDatProvinsi() async {
+    try {
+      Response response = await _dio.get(getProvinsi);
+      return response.data;
+    } catch (e) {
+      return e ;
+    }
+  }
+}
